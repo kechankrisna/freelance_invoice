b54ea094af2e45cac875db7550a53b3d31d7b695
kechankrisna <ke.chankrisna168@gmail.com>
2023-08-26 10:32:42 +0700

    info and pub

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -67,9 +67,6 @@ class _HomeScreenState extends State<HomeScreen> {
                         onPressed: printPreview, child: Text("print preview")),
                   ],
                   if (_excelInfo != null) Text(_excelInfo!),
-                  TextButton(
-                      onPressed: testLoadDummyExcel,
-                      child: Text("load dummy excel"))
                 ],
               ),
             ),
@@ -173,48 +170,4 @@ class _HomeScreenState extends State<HomeScreen> {
     }
     // print("content $content");
   }
-
-  testLoadDummyExcel() async {
-    var dir = await getApplicationCacheDirectory();
-    print("dir ${dir.path}");
-    final filePath = p.join(dir.path, "/assets/dummy.xlsx");
-    var content = await rootBundle.load("assets/dummy.xlsx");
-    // print(content.lengthInBytes);
-
-    // var file = io.File(filePath);
-    // print(file.absolute.path);
-    // var content = await file.readAsString();
-
-    // print("content ${content != null}");
-    // testReadExcel(content.buffer.asInt8List());
-  }
-
-  // testReadExcel(List<int> bytes) async {
-  //   var excel = Excel.decodeBytes(bytes);
-  //   var raws = excel.sheets[excel.getDefaultSheet()]!.rows;
-
-  //   final startRow = 6;
-  //   final datas = raws.sublist(6);
-  //   print(datas.length);
-
-  //   ///
-  //   var rows = <List>[];
-  //   for (var row in datas) {
-  //     var _singleData = [];
-  //     if (row.isPharsable) {
-  //       for (var e in row) {
-  //         _singleData.add(e?.value);
-  //       }
-  //       rows.add(_singleData);
-  //     }
-  //   }
-
-  //   final saleItems = rows.map((e) => SaleItem.fromRow(e)).toList();
-  //   print(saleItems[0]);
-  //   print(saleItems[1]);
-  //   final sales = RawSaleItemService.groupByInvoice(saleItems);
-  //   print("saleItems count ${saleItems.length}");
-  //   print("invoice count ${sales.length}");
-  //   print("last invoice count ${sales.last.invoice}");
-  // }
 }
